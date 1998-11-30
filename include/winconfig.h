979edbbc0be56b33080f8e2439f27700df7fdb07
Rob Earhart <earhart@cmu.edu>
1998-11-30 14:56:09 +0000

    Fixed winconfig.h

--- include/winconfig.h
+++ include/winconfig.h
@@ -1,3 +1,30 @@
+/* winconfig.h--SASL configuration for win32
+ * Ryan Troll
+ * $Id: winconfig.h,v 1.3 1998/11/30 14:56:10 rob Exp $
+ */
+/***********************************************************
+        Copyright 1998 by Carnegie Mellon University
+
+                      All Rights Reserved
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee is hereby granted,
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in
+supporting documentation, and that the name of Carnegie Mellon
+University not be used in advertising or publicity pertaining to
+distribution of the software without specific, written prior
+permission.
+
+CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE FOR
+ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+******************************************************************/
+
 #ifndef _WINCONFIG_H_
 #define _WINCONFIG_H_
 
@@ -7,7 +34,7 @@
 #define PACKAGE "cyrus-sasl"
 
 /* Our version */
-#define VERSION "v1.1b"
+#define VERSION "v1.2b3"
 
 /* Registry key that contains the locations of the plugins */
 #define SASL_KEY "SOFTWARE\\Carnegie Mellon\\Project Cyrus\\SASL Library\\Available Plugins"

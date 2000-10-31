f76c7543c47e953fcbd8b867fd550f16e8eab195
Larry Greenfield <leg@andrew.cmu.edu>
2000-10-31 20:27:03 +0000

    ready for 1.5.25

--- win32/include/config.h
+++ win32/include/config.h
@@ -50,7 +50,7 @@
 #define PACKAGE "cyrus-sasl"
 
 /* Our version */
-#define VERSION "1.5.24"
+#define VERSION "1.5.25"
 
 /* Registry key that contains the locations of the plugins */
 #define SASL_KEY "SOFTWARE\\Carnegie Mellon\\Project Cyrus\\SASL Library\\Available Plugins"

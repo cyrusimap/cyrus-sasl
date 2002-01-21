54b51ae1d3fefbac24e736e5494728729991f3c5
Rob Siemborski <rjs3@andrew.cmu.edu>
2002-01-21 16:56:29 +0000

    added testsaslauthd

---
+++ saslauthd/testsaslauthd.c
@@ -0,0 +1,307 @@
+/* test-saslauthd.c: saslauthd test utility
+ * Rob Siemborski
+ */
+/* 
+ * Copyright (c) 2002 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
+ *      Carnegie Mellon University
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <saslauthd.h>
+#include <stdio.h>
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+extern int errno;
+
+/*
+ * Keep calling the writev() system call with 'fd', 'iov', and 'iovcnt'
+ * until all the data is written out or an error occurs.
+ */
+static int retry_writev(int fd, struct iovec *iov, int iovcnt)
+{
+    int n;
+    int i;
+    int written = 0;
+    static int iov_max =
+#ifdef MAXIOV
+	MAXIOV
+#else
+#ifdef IOV_MAX
+	IOV_MAX
+#else
+	8192
+#endif
+#endif
+	;
+    
+    for (;;) {
+	while (iovcnt && iov[0].iov_len == 0) {
+	    iov++;
+	    iovcnt--;
+	}
+
+	if (!iovcnt) return written;
+
+	n = writev(fd, iov, iovcnt > iov_max ? iov_max : iovcnt);
+	if (n == -1) {
+	    if (errno == EINVAL && iov_max > 10) {
+		iov_max /= 2;
+		continue;
+	    }
+	    if (errno == EINTR) continue;
+	    return -1;
+	}
+
+	written += n;
+
+	for (i = 0; i < iovcnt; i++) {
+	    if (iov[i].iov_len > (unsigned) n) {
+		iov[i].iov_base = (char *)iov[i].iov_base + n;
+		iov[i].iov_len -= n;
+		break;
+	    }
+	    n -= iov[i].iov_len;
+	    iov[i].iov_len = 0;
+	}
+
+	if (i == iovcnt) return written;
+    }
+}
+
+
+/*
+ * Keep calling the read() system call with 'fd', 'buf', and 'nbyte'
+ * until all the data is read in or an error occurs.
+ */
+int retry_read(int fd, void *buf, unsigned nbyte)
+{
+    int n;
+    int nread = 0;
+
+    if (nbyte == 0) return 0;
+
+    for (;;) {
+	n = read(fd, buf, nbyte);
+	if (n == -1 || n == 0) {
+	    if (errno == EINTR || errno == EAGAIN) continue;
+	    return -1;
+	}
+
+	nread += n;
+
+	if (nread >= (int) nbyte) return nread;
+
+	buf += n;
+	nbyte -= n;
+    }
+}
+
+/* saslauthd-authenticated login */
+static int saslauthd_verify_password(const char *saslauthd_path,
+				   const char *userid, 
+				   const char *passwd,
+				   const char *service,
+				   const char *user_realm)
+{
+    static char response[1024];
+    int s;
+    struct sockaddr_un srvaddr;
+    int r;
+    unsigned short count;
+    void *context;
+    char pwpath[sizeof(srvaddr.sun_path)];
+    const char *p = NULL;
+
+    if(!service) service = "imap";
+    if(!user_realm) user_realm = "localhost";
+    if(!userid || !passwd) return -1;
+    
+    if (saslauthd_path) {
+	strncpy(pwpath, saslauthd_path, sizeof(pwpath));
+    } else {
+	if (strlen(PATH_SASLAUTHD_RUNDIR) + 4 + 1 > sizeof(pwpath))
+	    return -1;
+
+	strcpy(pwpath, PATH_SASLAUTHD_RUNDIR);
+	strcat(pwpath, "/mux");
+    }
+
+    s = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (s == -1) {
+	perror("socket() ");
+	return -1;
+    }
+
+    memset((char *)&srvaddr, 0, sizeof(srvaddr));
+    srvaddr.sun_family = AF_UNIX;
+    strncpy(srvaddr.sun_path, pwpath, sizeof(srvaddr.sun_path));
+
+    r = connect(s, (struct sockaddr *) &srvaddr, sizeof(srvaddr));
+    if (r == -1) {
+        perror("connect() ");
+	return -1;
+    }
+
+    /*
+     * build request of the form:
+     *
+     * count authid count password count service count realm
+     */
+    {
+ 	unsigned short u_len, p_len, s_len, r_len;
+ 	struct iovec iov[8];
+ 
+ 	u_len = htons(strlen(userid));
+ 	p_len = htons(strlen(passwd));
+	s_len = htons(strlen(service));
+	r_len = htons((user_realm ? strlen(user_realm) : 0));
+
+	iov[0].iov_base = &u_len;
+ 	iov[0].iov_len = sizeof(u_len);
+        iov[1].iov_base = (void*) userid;
+	iov[1].iov_len = strlen(userid);
+	iov[2].iov_base = &p_len;
+	iov[2].iov_len = sizeof(p_len);
+	iov[3].iov_base = (void*) passwd;
+	iov[3].iov_len = strlen(passwd);
+	iov[4].iov_base = &s_len;
+	iov[4].iov_len = sizeof(s_len);
+	iov[5].iov_base = (void*) service;
+	iov[5].iov_len = strlen(service);
+	iov[6].iov_base = &r_len;
+        iov[6].iov_len = sizeof(r_len);
+	iov[7].iov_base = user_realm ? (void*) user_realm : "";
+	iov[7].iov_len = user_realm ? strlen(user_realm) : 0;
+
+	if (retry_writev(s, iov, 8) == -1) {
+            fprintf(stderr,"write failed\n");
+  	    return -1;
+  	}
+    }
+  
+    /*
+     * read response of the form:
+     *
+     * count result
+     */
+    if (retry_read(s, &count, sizeof(count)) < (int) sizeof(count)) {
+        fprintf(stderr,"size read failed\n");
+	return -1;
+    }
+  
+    count = ntohs(count);
+    if (count < 2) { /* MUST have at least "OK" or "NO" */
+	close(s);
+        fprintf(stderr,"bad response from saslauthd\n");
+	return -1;
+    }
+  
+    count = (int)sizeof(response) < count ? sizeof(response) : count;
+    if (retry_read(s, response, count) < count) {
+	close(s);
+        fprintf(stderr,"read failed\n");
+	return -1;
+    }
+  
+    close(s);
+  
+    if (!strncmp(response, "OK", 2)) {
+	fprintf(stderr, "Success.\n");
+	return 0;
+    }
+  
+    response[count] = '\0';
+    fprintf(stderr,"authentication failed\n");
+    return -1;
+}
+
+int
+main(int argc, char *argv[])
+{
+  const char *user = NULL, *password = NULL;
+  const char *realm = NULL, *service = NULL, *path = NULL;
+  int c;
+  int flag_error = 0;
+  unsigned passlen, verifylen;
+  const char *errstr = NULL;
+  int result;
+  char *user_domain = NULL;
+
+  while ((c = getopt(argc, argv, "p:u:r:s:f:")) != EOF)
+      switch (c) {
+      case 'f':
+	  path = optarg;
+	  break;
+      case 's':
+	  service = optarg;
+	  break;
+      case 'r':
+	  realm = optarg;
+	  break;
+      case 'u':
+	  user = optarg;
+	  break;
+      case 'p':
+	  password = optarg;
+	  break;
+      default:
+	  flag_error = 1;
+	  break;
+    }
+
+  if (!user || !password)
+    flag_error = 1;
+
+  if (flag_error) {
+    (void)fprintf(stderr,
+		 "%s: usage: %s -u username -p password\n"
+		 "              [-r realm] [-s servicename] [-f socket path]\n",
+		  argv[0], argv[0]);
+    exit(1);
+  }
+
+  /* saslauthd-authenticated login */
+  return saslauthd_verify_password(path, user, password, service, realm);
+}
+

b4047d9b4ecb4a00000499f3a2d46bf318562bf9
Rob Siemborski <rjs3@andrew.cmu.edu>
2002-08-09 16:36:19 +0000

    ready for 2.1.7

--- win32/include/config.h
+++ win32/include/config.h
@@ -50,7 +50,7 @@
 #define PACKAGE "cyrus-sasl"
 
 /* Our version */
-#define VERSION "2.1.6"
+#define VERSION "2.1.7"
 
 /* Registry key that contains the locations of the plugins */
 #define SASL_KEY "SOFTWARE\\Carnegie Mellon\\Project Cyrus\\SASL Library\\Available Plugins"

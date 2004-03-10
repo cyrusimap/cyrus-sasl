17a6bc16296cc95c0850ff415439aa8193d621f8
Ken Murchison <murch@andrew.cmu.edu>
2004-03-10 15:53:10 +0000

    fix typo

--- include/sasl.h
+++ include/sasl.h
@@ -627,7 +627,7 @@ LIBSASL_API void sasl_version(const char **implementation,
 
 /* Extended version of sasl_version().
  *
- * This function is to be used for
+ * This function is to be used
  *  for library version display and logging
  *  for bug workarounds in old library versions
  *

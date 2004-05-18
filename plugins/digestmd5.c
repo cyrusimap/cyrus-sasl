a554fdc7c6657b04e204191da5f6d624022f04af
Ken Murchison <murch@andrew.cmu.edu>
2004-05-18 15:08:40 +0000

    made the DIGEST-MD5 global context a struct containing the reauth_cache so we can NULL it after we free it

--- plugins/digestmd5.c
+++ plugins/digestmd5.c
@@ -3,7 +3,7 @@
  * Rob Siemborski
  * Tim Martin
  * Alexey Melnikov 
- * $Id: digestmd5.c,v 1.167 2004/02/23 22:03:09 rjs3 Exp $
+ * $Id: digestmd5.c,v 1.168 2004/05/18 15:08:41 ken3 Exp $
  */
 /* 
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
@@ -120,7 +120,7 @@ extern int      gethostname(char *, int);
 
 /*****************************  Common Section  *****************************/
 
-static const char plugin_id[] = "$Id: digestmd5.c,v 1.167 2004/02/23 22:03:09 rjs3 Exp $";
+static const char plugin_id[] = "$Id: digestmd5.c,v 1.168 2004/05/18 15:08:41 ken3 Exp $";
 
 /* Definitions */
 #define NONCE_SIZE (32)		/* arbitrary */
@@ -200,6 +200,11 @@ typedef struct reauth_cache {
 
     reauth_entry_t *e;		/* fixed-size hash table of entries */
 } reauth_cache_t;
+
+/* global context for reauth use */
+struct digest_glob_context { 
+   reauth_cache_t *reauth; 
+} my_glob_context;
 
 /* context that stores info */
 typedef struct context {
@@ -1500,7 +1505,9 @@ static void clear_reauth_entry(reauth_entry_t *reauth, enum Context_type type,
 static void digestmd5_common_mech_free(void *glob_context,
 				       const sasl_utils_t *utils)
 {
-    reauth_cache_t *reauth_cache = (reauth_cache_t *) glob_context;
+    struct digest_glob_context *my_glob_context =
+	(struct digest_glob_context *) glob_context;
+    reauth_cache_t *reauth_cache = my_glob_context->reauth;
     size_t n;
     
     if (!reauth_cache) return;
@@ -1512,6 +1519,7 @@ static void digestmd5_common_mech_free(void *glob_context,
     if (reauth_cache->mutex) utils->mutex_free(reauth_cache->mutex);
 
     utils->free(reauth_cache);
+    my_glob_context->reauth = NULL;
 }
 
 /*****************************  Server Section  *****************************/
@@ -1712,7 +1720,7 @@ static int digestmd5_server_mech_new(void *glob_context,
     
     text->state = 1;
     text->i_am = SERVER;
-    text->reauth = glob_context;
+    text->reauth = ((struct digest_glob_context *) glob_context)->reauth;
     
     *conn_context = text;
     return SASL_OK;
@@ -2608,7 +2616,7 @@ static sasl_server_plug_t digestmd5_server_plugins[] =
 	| SASL_SEC_NOANONYMOUS
 	| SASL_SEC_MUTUAL_AUTH,		/* security_flags */
 	SASL_FEAT_ALLOWS_PROXY,		/* features */
-	NULL,				/* glob_context */
+	&my_glob_context,		/* glob_context */
 	&digestmd5_server_mech_new,	/* mech_new */
 	&digestmd5_server_mech_step,	/* mech_step */
 	&digestmd5_server_mech_dispose,	/* mech_dispose */
@@ -2664,7 +2672,7 @@ int digestmd5_server_plug_init(sasl_utils_t *utils,
 	memset(reauth_cache->e, 0, reauth_cache->size * sizeof(reauth_entry_t));
     }
 
-    digestmd5_server_plugins[0].glob_context = reauth_cache;
+    ((struct digest_glob_context *) digestmd5_server_plugins[0].glob_context)->reauth = reauth_cache;
 
     *out_version = SASL_SERVER_PLUG_VERSION;
     *pluglist = digestmd5_server_plugins;
@@ -3550,7 +3558,7 @@ static int digestmd5_client_mech_new(void *glob_context,
     
     text->state = 1;
     text->i_am = CLIENT;
-    text->reauth = glob_context;
+    text->reauth = ((struct digest_glob_context *) glob_context)->reauth;
     
     *conn_context = text;
 
@@ -3907,7 +3915,7 @@ static sasl_client_plug_t digestmd5_client_plugins[] =
 	SASL_FEAT_NEEDSERVERFQDN
 	| SASL_FEAT_ALLOWS_PROXY, 	/* features */
 	NULL,				/* required_prompts */
-	NULL,				/* glob_context */
+	&my_glob_context,		/* glob_context */
 	&digestmd5_client_mech_new,	/* mech_new */
 	&digestmd5_client_mech_step,	/* mech_step */
 	&digestmd5_client_mech_dispose,	/* mech_dispose */
@@ -3949,7 +3957,7 @@ int digestmd5_client_plug_init(sasl_utils_t *utils,
 	return SASL_NOMEM;
     memset(reauth_cache->e, 0, reauth_cache->size * sizeof(reauth_entry_t));
 
-    digestmd5_client_plugins[0].glob_context = reauth_cache;
+    ((struct digest_glob_context *) digestmd5_client_plugins[0].glob_context)->reauth = reauth_cache;
 
     *out_version = SASL_CLIENT_PLUG_VERSION;
     *pluglist = digestmd5_client_plugins;

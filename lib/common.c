7d617b0394997f10af6da234620f7746d00136ad
Derrick Brashear <shadow@dementia.org>
2004-09-22 21:35:34 +0000

    don't honor SASL_PATH in setuid environment. from Gentoo.

--- lib/common.c
+++ lib/common.c
@@ -1,7 +1,7 @@
 /* common.c - Functions that are common to server and clinet
  * Rob Siemborski
  * Tim Martin
- * $Id: common.c,v 1.103 2004/07/06 16:03:05 rjs3 Exp $
+ * $Id: common.c,v 1.104 2004/09/22 21:35:34 shadow Exp $
  */
 /* 
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
@@ -1838,7 +1838,10 @@ _sasl_getpath(void *context __attribute__((unused)),
   if (! path)
     return SASL_BADPARAM;
 
-  *path = getenv(SASL_PATH_ENV_VAR);
+  /* Honor external variable only in a safe environment */
+  if (getuid() == geteuid() && getgid() == getegid())
+    *path = getenv(SASL_PATH_ENV_VAR);
+
   if (! *path)
     *path = PLUGINDIR;
 

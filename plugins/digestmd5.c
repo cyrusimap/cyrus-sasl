e6d893478d8370ad82e4774572c4619e306e42d5
Alexey Melnikov <alexey.melnikov@isode.com>
2012-04-25 15:34:28 +0100

    Removed some trailing whitespaces GIT complains about

--- plugins/digestmd5.c
+++ plugins/digestmd5.c
@@ -198,7 +198,7 @@ typedef struct reauth_entry {
 
 	    /* for HTTP mode (RFC 2617) only */
 	    char *algorithm;
-	    unsigned char *opaque;	
+	    unsigned char *opaque;
 	} c; /* client stuff */
     } u;
 } reauth_entry_t;

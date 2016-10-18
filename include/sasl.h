88d82a310f69ce5e0e77d7771aefaa5b912d49bb
Ken Murchison <murch@andrew.cmu.edu>
2016-10-18 13:14:48 -0400

    Getting ready for 2.1.27

--- include/sasl.h
+++ include/sasl.h
@@ -126,7 +126,7 @@
 /* Keep in sync with win32/common.mak */
 #define SASL_VERSION_MAJOR 2
 #define SASL_VERSION_MINOR 1
-#define SASL_VERSION_STEP 26
+#define SASL_VERSION_STEP 27
 
 /* A convenience macro: same as was defined in the OpenLDAP LDAPDB */
 #define SASL_VERSION_FULL ((SASL_VERSION_MAJOR << 16) |\

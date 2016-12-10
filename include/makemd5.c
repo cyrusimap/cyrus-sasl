88cda9d196cbf28e66b986980ddd5a7d8a372de0
Ken Murchison <murch@andrew.cmu.edu>
2016-12-10 11:41:21 -0500

    include/: removed legacy $Id$ strings and updated CMU Copyright

--- include/makemd5.c
+++ include/makemd5.c
@@ -1,10 +1,9 @@
 /* creates the md5global.h file. 
  *  Derived from KTH kerberos library bits.c program
  * Tim Martin 
- * $Id: makemd5.c,v 1.4 2003/02/13 19:55:52 rjs3 Exp $
  */
 /* 
- * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
+ * Copyright (c) 1998-2016 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -22,12 +21,13 @@
  *    endorse or promote products derived from this software without
  *    prior written permission. For permission or any other legal
  *    details, please contact  
- *      Office of Technology Transfer
  *      Carnegie Mellon University
- *      5000 Forbes Avenue
- *      Pittsburgh, PA  15213-3890
- *      (412) 268-4387, fax: (412) 268-7395
- *      tech-transfer@andrew.cmu.edu
+ *      Center for Technology Transfer and Enterprise Creation
+ *      4615 Forbes Avenue
+ *      Suite 302
+ *      Pittsburgh, PA  15213
+ *      (412) 268-7393, fax: (412) 268-7395
+ *      innovation@andrew.cmu.edu
  *
  * 4. Redistributions of any form whatsoever must retain the following
  *    acknowledgment:

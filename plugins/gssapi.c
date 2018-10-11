c34f4bd638a530c5de37ca32f10bd369079eafe1
Jakub Jelen <jjelen@redhat.com>
2018-10-11 17:19:35 +0200

    plugins/gssapi: Drop dead code

--- plugins/gssapi.c
+++ plugins/gssapi.c
@@ -1695,8 +1695,7 @@ static int gssapi_client_mech_step(void *conn_context,
 		/* make the prompt list */
 		int result =
 		    _plug_make_prompts(params->utils, prompt_need,
-				       user_result == SASL_INTERACT ?
-				       "Please enter your authorization name" : NULL, NULL,
+				       "Please enter your authorization name", NULL,
 				       NULL, NULL,
 				       NULL, NULL,
 				       NULL, NULL, NULL,

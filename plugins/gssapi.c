a6264b59a9b052d6e66119490c71f3c5e8689532
Joakim Ekblad <aio.github@aio.nu>
2020-02-05 07:21:28 +0100

    plugins/gssapi: Intentially broken indentation to reduce diff
    Signed-off-by: Joakim Ekblad <aio.github@aio.nu>

--- plugins/gssapi.c
+++ plugins/gssapi.c
@@ -2046,7 +2046,7 @@ static int gssapi_client_mech_step(void *conn_context,
 		
 		return SASL_INTERACT;
 	    }
-        }
+	}
 	    
 	if (text->server_name == GSS_C_NO_NAME) { /* only once */
 	    if (params->serverFQDN == NULL

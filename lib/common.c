87a26d50c7da4e1d1e10f2a161930fe9b92116b1
Howard Chu <hyc@symas.com>
2021-09-22 16:06:58 +0100

    Re-fix _sasl_add_string #587
    Just a cleaned up version of the fix.
    
    Signed-off-by: Howard Chu <hyc@symas.com>

--- lib/common.c
+++ lib/common.c
@@ -190,12 +190,12 @@ int _sasl_add_string(char **out, size_t *alloclen,
 
   if (add==NULL) add = "(null)";
 
-  addlen=strlen(add)+1; /* only compute once */
-  if (_buf_alloc(out, alloclen, (*outlen)+addlen)!=SASL_OK)
+  addlen=strlen(add); /* only compute once */
+  if (_buf_alloc(out, alloclen, (*outlen)+addlen+1)!=SASL_OK)
     return SASL_NOMEM;
 
-  strncpy(*out + *outlen, add, addlen);
-  *outlen += addlen-1;
+  strcpy(*out + *outlen, add);
+  *outlen += addlen;
 
   return SASL_OK;
 }

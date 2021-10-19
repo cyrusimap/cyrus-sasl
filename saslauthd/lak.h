1d98610dd0c1dad1c2a978256a5967c463197495
Quanah Gibson-Mount <quanah@symas.com>
2021-10-19 16:10:46 +0000

    For cyrusimap#684 - Update code for HAVE_SYS_TIME_H
    Signed-off-by: Quanah Gibson-Mount <quanah@symas.com>

--- saslauthd/lak.h
+++ saslauthd/lak.h
@@ -31,16 +31,10 @@
 #include <ldap.h>
 #include <lber.h>
 
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
+#if HAVE_SYS_TIME_H
 #  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
+# include <time.h>
 
 #define LAK_OK 0
 #define LAK_FAIL -1

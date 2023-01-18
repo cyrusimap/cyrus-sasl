f2446064d27ddbca8046cc092bd5b06085e0e651
stf <7o5rfu92t@ctrlc.hu>
2023-01-18 20:07:49 +0100

    added licenses to the files not having them
    Signed-off-by: stf <7o5rfu92t@ctrlc.hu>

--- plugins/opaque.c
+++ plugins/opaque.c
@@ -1,3 +1,21 @@
+/*
+    @copyright 2018-21, opaque@ctrlc.hu
+    This file is part of the cyrus-sasl opaque mechansim.
+
+    The cyrus-sasl opaque mechanism is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    The cyrus-sasl opaque mechanism is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with The cyrus-sasl opaque mechanism. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <opaque.h>
 #include <string.h>
 #include "plugin_common.h"

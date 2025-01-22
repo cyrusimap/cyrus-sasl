b8302a8c99d7079d1b68acf18802fce25c8c8243
ellie timoney <ellie@fastmail.com>
2025-01-22 13:53:14 +1100

    docsrc/conf.py: cyrus-imapd 3.8.5 and 3.10.1 released

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -416,18 +416,18 @@ epub_exclude_files = ['search.html']
 # When this is updated, you may also need to update the version and release
 # definitions listed above to stay up to date.
 rst_prolog = """
-.. |imap_last_stable_version| replace:: 3.8.4
+.. |imap_last_stable_version| replace:: 3.8.5
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-3.8`
-.. |imap_last_stable_next_version| replace:: 3.8.4 + patches
-.. |imap_current_stable_version| replace:: 3.10.0
+.. |imap_last_stable_next_version| replace:: 3.8.5 + patches
+.. |imap_current_stable_version| replace:: 3.10.1
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-3.10`
-.. |imap_current_stable_next_version| replace:: 3.10.0 + patches
+.. |imap_current_stable_next_version| replace:: 3.10.1 + patches
 .. |imap_latest_development_version| replace:: 3.11.0-alpha0
 .. |imap_latest_development_branch| replace:: master
 .. |sasl_current_stable_version| replace:: 2.1.28
 .. |imap_stable_release_notes| raw:: html
 
-    <a href="3.10/x/3.10.0.html">3.10.0</a>
+    <a href="3.10/x/3.10.1.html">3.10.1</a>
 
 .. |imap_development_release_notes| raw:: html
 
